f47870f0b65245a826e629f4b467341984b90e0a
jarvon <jarvondigital@gmail.com>
2023-04-14 12:13:37 -0400

    Added Text Replacer

--- src/app/@services/subtitles/subtitles.service.ts
+++ src/app/@services/subtitles/subtitles.service.ts
@@ -1,5 +1,5 @@
 import {inject, Injectable} from '@angular/core';
-import {collection, Firestore, getDocs} from "@angular/fire/firestore";
+import {collection, doc, Firestore, getDoc, getDocs} from "@angular/fire/firestore";
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +9,6 @@ export class SubtitlesService {
   private firestore: Firestore = inject(Firestore);
 
   async getAllSubtitles() {
-
     try {
       let docQuery = await getDocs(collection(this.firestore, 'subtitles'))
       return docQuery.docs.map((sub) => sub.data())
@@ -17,6 +16,15 @@ export class SubtitlesService {
       console.log(err);
       return false;
     }
+  }
 
+  async getAllReplacers() {
+    try {
+      let docQuery = await getDoc(doc(this.firestore, 'system', 'settings'))
+      return docQuery.data()?.['replacers'] as any[];
+    } catch (err) {
+      console.log(err)
+      return [];
+    }
   }
 }
--- src/app/@views/dashboard/@views/compare/compare.component.ts
+++ src/app/@views/dashboard/@views/compare/compare.component.ts
@@ -2,6 +2,11 @@ import {Component, inject, OnDestroy, OnInit} from '@angular/core';
 import {SubtitlesService} from "../../../../@services/subtitles/subtitles.service";
 import {doc, Firestore, updateDoc} from "@angular/fire/firestore";
 
+interface replacer {
+  search: string[],
+  replacers: string[]
+}
+
 @Component({
   selector: 'app-compare',
   templateUrl: './compare.component.html',
@@ -16,18 +21,25 @@ export class CompareComponent implements OnInit, OnDestroy {
 
   autoSave: any;
 
+  searchString = ""
+  replacerString = "";
+  replacers: replacer[] = [
+    {search: [], replacers: []}
+  ];
+
   async ngOnInit(): Promise<void> {
 
-    this.subtitles = await this.subtitleService.getAllSubtitles()
+    this.subtitles = await this.subtitleService.getAllSubtitles();
+    this.replacers = await this.subtitleService.getAllReplacers();
 
     setInterval(async () => {
       if (this.workingFile) {
         await this.saveFile()
       } else {
-        console.log('Autosave Pending Document. Will try again in 30 seconds.')
+        console.log('Autosave Pending Document. Will try again in 10 seconds.')
       }
 
-    }, 15000)
+    }, 10000)
 
   }
 
@@ -46,5 +58,61 @@ export class CompareComponent implements OnInit, OnDestroy {
       console.log(err)
     }
 
+  }
+
+  async addReplacement() {
+
+    this.replacers.push({
+      search: [this.searchString.toUpperCase(), (this.searchString.charAt(0).toUpperCase() + this.searchString.slice(1).toLowerCase()), this.searchString.toLowerCase()],
+      replacers: [this.replacerString.toUpperCase(), (this.replacerString.charAt(0).toUpperCase() + this.replacerString.slice(1).toLowerCase()), this.replacerString.toLowerCase()]
+    })
+
+    await this.saveReplacerDoc();
+
+    this.searchString = "";
+    this.replacerString = "";
+
+  }
+
+  async removeReplacer(i: number) {
+
+    if(window.confirm("Are you sure you want to delete this replacer?")) {
+      this.replacers.splice(i, 1);
+      await this.saveReplacerDoc();
+    }
+
+  }
+
+  private async saveReplacerDoc() {
+    await updateDoc(doc(this.firestore, "system", "settings"), {
+      replacers: this.replacers
+    })
+  }
+
+  async replaceAllText() {
+    if(!this.workingFile) return;
+    let isConfirmed = window.confirm("Your are about to replace some or all spanish translation with english text. Do you want to proceed with this action?");
+    let replaced: any[] = [];
+
+    if(isConfirmed) {
+
+      this.workingFile.subtitles.forEach((sub: any) => {
+        let utterance: any = sub.languages.es
+        this.replacers.forEach(rep => {
+          if(utterance.toUpperCase().includes(rep.search[0])) {
+            rep.search.forEach((alt, i) => {
+              sub.languages.es = sub.languages.es.replaceAll(rep.search[i], rep.replacers[i])
+            })
+            replaced.push(utterance, sub.languages.es);
+            console.log(sub.languages.es);
+          }
+        })
+      })
+
+      this.workingFile.markForCheck();
+      window.alert("Text have been successfully replaced!");
+      await this.saveFile()
+
+    }
   }
 }
